789532a62695f834d5f65ebb690dca16ac444aae
kurotamashi30 <kurotamashi30@gmail.com>
2023-12-29 00:36:38 +0900

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import{useState} from 'react'
 
 function App() {
+  const TopComponent =()=>{
+    const[ state, setState] = useState();
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MiddleDomponent state = {state} />
     </div>
   );
 }
+const MiddleDomponent = (state)=>{
+  return(
+    <div>
+      <BottomComponent state = {state} />
+    </div>
+  )}
+  const BottomDomponetn =(state)=>{
+    return <div>{state}</div>
+  }
 
 export default App;
