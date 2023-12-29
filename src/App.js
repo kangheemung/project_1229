a206511c1998da185287e05041872d292f6ede1e
kurotamashi30 <kurotamashi30@gmail.com>
2023-12-29 23:50:35 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Example from './components/Example.js';
 import {Header} from './Header';
 import {Home} from './pages/Home';
 import {Menu} from './pages/Menu';
 import {Profile} from './pages/Profile';
 import {Contact} from './pages/Contact';
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Assuming the directory is "components"
 
 function App() {
+ const client = new QueryClient();
   const [state] = useState('');
 
   return (
-    <div className="App">
-      <Router>
-        <Header  bg="dark" data-bs-theme="dark"/>
-        <Routes>
-          <Route path="/"  element ={<Home />}/>
-          <Route path="/profile" element ={<Profile />}/>
-          <Route path="/menu" element ={<Menu />}/>
-          <Route path="/contact"  element ={<Contact />}/>
-          <Route path="*"  element ={<h1>page no found</h1>}/>
-        </Routes>
-      </Router>
+    <div className = "App">
+      <QueryClientProvider client = {client}>
+        <Router>
+          <Header  bg="dark" data-bs-theme="dark"/>
+          <Routes>
+            <Route path="/"  element ={<Home />}/>
+            <Route path="/profile" element ={<Profile />}/>
+            <Route path="/menu" element ={<Menu />}/>
+            <Route path="/contact"  element ={<Contact />}/>
+            <Route path="*"  element ={<h1>page no found</h1>}/>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
       <TopComponent state={state} />
       <Example />
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
-export const Home=()=> {
+export const Home = () => {
+  // Fetch cat fact using react-query with the object syntax
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["cat"],
+    queryFn: () => Axios.get("https://catfact.ninja/fact").then((res) => res.data),
+  });
+
+  // Handle loading and error states
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+  // Render data
   return (
     <div>
       <h1>Home</h1>
+      <h1>Cat Fact: {data?.fact}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
